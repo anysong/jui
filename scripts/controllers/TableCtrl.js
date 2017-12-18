c46e2105a013ddfc6b1e8eeb044454fd15cd89b0
anysong <985774505@qq.com>
2017-12-18 17:09:45 +0800

    dev

--- scripts/controllers/TableCtrl.js
+++ scripts/controllers/TableCtrl.js
@@ -1,48 +1,74 @@
 angular.module('jui').controller('TableCtrl', ['$scope', function($scope){
 	$scope.workList = [1,2,3,4,5,6,7,8,9];
-
+    $scope.hoverIndex = null;
 	var bind = function(){
 		//body-height
         var tar = null;
-        $('.zc-j-fixed-right').on('mouseenter', function() {
+        $('.jui-table-fixed-right').on('mouseenter', function() {
             tar = 'right';
         });
-        $('.zc-j-fixed-right').on('mouseleave', function() {
+        $('.jui-table-fixed-right').on('mouseleave', function() {
             tar = 'main';
         });
-        $('.zc-j-fixed-main-body').on('scroll', function(ev) {
+        $('.jui-table-fixed-main-body').on('scroll', function(ev) {
             if (!tar) tar = 'main';
             if (tar == 'right') return;
-            $('.zc-j-fixed-right-body-inner').scrollTop(ev.target.scrollTop);
-            $('.zc-j-fixed-main-header').scrollLeft(ev.target.scrollLeft);
+            $('.jui-table-fixed-right-body-inner').scrollTop(ev.target.scrollTop);
+            $('.jui-table-fixed-main-header').scrollLeft(ev.target.scrollLeft);
             if ((ev.target.scrollLeft + ev.target.clientWidth + 1) >= ev.target.scrollWidth) {
-                $('.zc-j-fixed-right').removeClass('zc-j-fixed-right-shadow');
+                $('.jui-table-fixed-right').removeClass('jui-table-fixed-right-shadow');
             } else {
-                $('.zc-j-fixed-right').addClass('zc-j-fixed-right-shadow');
+                $('.jui-table-fixed-right').addClass('jui-table-fixed-right-shadow');
             };
         });
-        $('.zc-j-fixed-right-body-inner').on('scroll', function(ev) {
+        $('.jui-table-fixed-right-body-inner').on('scroll', function(ev) {
             if (!tar) tar = 'right';
             if (tar == 'main') return;
-            $('.zc-j-fixed-main-body').scrollTop(ev.target.scrollTop);
+            $('.jui-table-fixed-main-body').scrollTop(ev.target.scrollTop);
         });
         //横向
-        $('.zc-j-fixed-main-header').scroll(function(ev) {
-            $('.zc-j-fixed-main-body').scrollLeft(ev.target.scrollLeft);
+        $('.jui-table-fixed-main-header').scroll(function(ev) {
+            $('.jui-table-fixed-main-body').scrollLeft(ev.target.scrollLeft);
         });
 
         //height
-        $('.zc-j-fixed-inner').css('max-height', document.body.clientHeight - 300 + 'px');
+        $('.js-inner').css('max-height', document.body.clientHeight - 300 + 'px');
         window.onresize = function() {
-            $('.zc-j-fixed-inner').css('max-height', document.body.clientHeight - 300 + 'px')
+            $('.js-inner').css('max-height', document.body.clientHeight - 300 + 'px')
         };
 
         var userAgent = window.navigator.userAgent;
         if (userAgent.indexOf('Firefox') != -1) {
-            $('.zc-j-fixed-main-header').css('margin-bottom', '-16px');
-            $('.zc-j-fixed-right-header').css('margin-bottom', '-16px');
-            $('.zc-j-fixed-right-body').css('margin-bottom', '-16px');
+            $('.jui-table-fixed-main-header').css('margin-bottom', '-16px');
+            $('.jui-table-fixed-right-header').css('margin-bottom', '-16px');
+            $('.jui-table-fixed-right-body').css('margin-bottom', '-16px');
+        };
+    };
+    var addHoverStyle = function(item, index){
+        // $scope.hoverId = item.id;
+        $scope.hoverIndex = index + 1;
+        console.log(index)
+    };
+    var removeHoverStyle = function(item, index){
+        $scope.hoverIndex = null;
+    };
+    var initFunc = function(){
+        $scope.addHoverStyle = addHoverStyle;
+        $scope.removeHoverStyle = removeHoverStyle;
+    };
+    //ui-table-fixed-right
+    var initShadow = function(){
+        var scrollLeft = $('.jui-table-fixed-main-body').scrollLeft;
+        var clientWidth = $('.jui-table-fixed-main-body').clientWidth;
+       
+        return;
+        if ((ev.target.scrollLeft + ev.target.clientWidth + 1) >= ev.target.scrollWidth) {
+            $('.jui-table-fixed-right').removeClass('jui-table-fixed-right-shadow');
+        } else {
+            $('.jui-table-fixed-right').addClass('jui-table-fixed-right-shadow');
         };
     };
     bind();
+    initFunc();
+    initShadow();
 }])
