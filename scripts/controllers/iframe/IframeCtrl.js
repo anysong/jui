adfe29c9aa43b5501ee290ce9e82cc1d5d909bbf
anysong <985774505@qq.com>
2018-01-17 18:36:51 +0800

    dev

--- scripts/controllers/iframe/IframeCtrl.js
+++ scripts/controllers/iframe/IframeCtrl.js
@@ -1,6 +1,7 @@
 angular.module('jui').controller('IframeCtrl', ['$scope','$sce', function($scope, $sce){
 	var initParams = function(){
-		$scope.iframeList = [{
+		$scope.iframeList = [
+		{
 			'name': '01',
 			'src': $sce.trustAsResourceUrl('https://www.sobot.com')
 		},{
@@ -9,6 +10,15 @@ angular.module('jui').controller('IframeCtrl', ['$scope','$sce', function($scope
 		},{
 			'name': '03',
 			'src': $sce.trustAsResourceUrl('http://www.baidu.com')
+		},{
+			'name': '聊',
+			'src': $sce.trustAsResourceUrl('https://www.sobot.com/chat/pc/index.html?sysNum=a741c73459dd4870914280c9bfa241e9')
+		},{
+			'name': 'app',
+			'src': $sce.trustAsResourceUrl('https://www.sobot.com/chat/h5/index.html?sysNum=a741c73459dd4870914280c9bfa241e9&source=2')
+		},{
+			'name': 'v',
+			'src': $sce.trustAsResourceUrl('https://www.sobot.com/chat/h5/index.html?sysNum=a741c73459dd4870914280c9bfa241e9&source=1')
 		}];
 		$scope.activeIrame = $scope.iframeList[0];
 		console.log($scope.activeIrame);
